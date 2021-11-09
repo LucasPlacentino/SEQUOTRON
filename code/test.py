f6358bb6a7a1237bfc567dbfc8b3509d0cfd49e9
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2021-11-09 12:43:01 +0100

    added tempo and clock

--- code/test.py
+++ code/test.py
@@ -1,1 +1,36 @@
-print("test")
+"""
+TRANH201INFO3 sequencer test code
+"""
+
+import gpiozero
+import time
+
+class Tempo:
+    # Tempo
+    current_tempo = 60
+    def changeTempo(self, nouveau_tempo):
+        self.current_tempo=nouveau_tempo
+
+tempo = Tempo() # Tempo initialization
+
+class Clock:
+    # Clock
+    def clocking(self):
+        print("tic")
+        clock=0
+        time.sleep(60/tempo.current_tempo)
+        print("tac")
+        clock=1
+        time.sleep(60/tempo.current_tempo)
+
+clock = Clock() # Clock initialization
+
+for i in range(1,10):
+    # Start of the tic tac sequence (10 times)
+    clock.clocking()
+
+
+
+
+#led1 = gpiozero.LED(17)
+
