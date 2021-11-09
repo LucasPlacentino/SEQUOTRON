ecd83a7ef3350711bec145426d6cdbc31265a8be
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2021-11-09 12:48:19 +0100

    rectification

--- code/test.py
+++ code/test.py
@@ -15,19 +15,19 @@ tempo = Tempo() # Tempo initialization
 
 class Clock:
     # Clock
-    def clocking(self):
-        print("tic")
+    def ticking(self):
+        print("tick")
         clock=0
         time.sleep(60/tempo.current_tempo)
-        print("tac")
+        print("tack")
         clock=1
         time.sleep(60/tempo.current_tempo)
 
 clock = Clock() # Clock initialization
 
 for i in range(1,10):
-    # Start of the tic tac sequence (10 times)
-    clock.clocking()
+    # Start of the tick tack sequence (10 times)
+    clock.ticking()
 
 
 
