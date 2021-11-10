2b51957000f4a2af7d0bfdb7df8b960c374c1944
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2021-11-10 13:24:40 +0100

    update 10/11 réu
    aide de Cédric

--- code/test.py
+++ code/test.py
@@ -7,27 +7,35 @@ import time
 
 class Tempo:
     # Tempo
-    current_tempo = 60
+    def __init__(self, current_tempo):
+        self.current_tempo = current_tempo
     def changeTempo(self, nouveau_tempo):
         self.current_tempo=nouveau_tempo
 
-tempo = Tempo() # Tempo initialization
-
 class Clock:
     # Clock
+    def __init__(self, tempo):
+        self.tempo = tempo
     def ticking(self):
         print("tick")
         clock=0
-        time.sleep(60/tempo.current_tempo)
+        time.sleep(60/self.tempo.current_tempo)
         print("tack")
         clock=1
-        time.sleep(60/tempo.current_tempo)
+        time.sleep(60/self.tempo.current_tempo)
 
-clock = Clock() # Clock initialization
 
-for i in range(1,10):
-    # Start of the tick tack sequence (10 times)
-    clock.ticking()
+def main():
+    tempo = Tempo(60) # Tempo initialization
+    clock = Clock(tempo) # Clock initialization
+
+    for i in range(1,10):
+        # Start of the tick tack sequence (10 times)
+        clock.ticking()
+
+if __name__ == "__main__":
+    main()
+
 
 
 
