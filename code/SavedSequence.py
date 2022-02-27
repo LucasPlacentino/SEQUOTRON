ac5644a7787e03bd32a39c433618cc45e264424f
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2022-02-27 14:44:17 +0100

    added SavedSequence.py
    saves or loads a sequence to/from a file

---
+++ code/SavedSequence.py
@@ -0,0 +1,17 @@
+# translated to EN
+
+#* --- Saves a sequence or loads a saved one ---
+
+def read_sequences(file):
+    p = []
+    with open(file, encoding='utf-8') as f:
+        for i in f:
+            a = i.split()
+            p.append(a)
+    return p
+
+def write_sequences(l, file):
+    with open(file, 'w', encoding='utf-8') as f:
+        for i in range(len(l)):
+            f.write(l[i][0] + ' ' + l[i][1] + '\n')
+    
