ba13262cd8bb4e5eedd5bf8c577d43cd1b24ae2f
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2022-03-15 15:55:48 +0100

    added test

--- code/SequencerMainTest.py
+++ code/SequencerMainTest.py
@@ -2,12 +2,9 @@
 TRANH201INFO3 sequencer test code
 """
 
-#* import gpiozero # ?
-from gpiozero import Device #*
-from gpiozero.pins.mock import MockFactory #*
+# import gpiozero
 import time
 import sys
-import threading
 
 # from Step import Step #Step class
 from LCDSequencer import LCDSequencer #LCD class
@@ -23,29 +20,23 @@ from Env import SIMULATED_LCD, SIMULATED_DAC
 
 
 def main(): # Main function, activated when sequencer launched
-
-    # ! -------------------
-    # ! here below is for setting the pins from gpiozero as fake pins for testing purposes
-    #gpiozero.Device.pin_factory = gpiozero.pins.mock.MockFactory()
-    Device.pin_factory = MockFactory()
-    print("------!-------MOCK PINS------!-------")
-    # ! -------------------
+    
     global sequencer
     sequencer = Sequencer()
     global lcd
     if SIMULATED_LCD:
         lcd = Simulatelcd()
     else:
-        #lcd = LCDSequencer()
-        print("uevbu")
+        lcd = LCDSequencer()
     lcd.toggleBacklight(True)
     #lcd.test()
-
+    #time.sleep(5)
+    
     global tempo
     tempo = Tempo(60, lcd) # initial tempo is 60 bpm
     global gate
     gate = Gate(sequencer.dac2, 0, lcd)
-
+    
     #init_tempo = tempo.value
     #lcd.displayTempo(init_tempo)
     init_step = sequencer.noteSequence.step
@@ -69,11 +60,10 @@ def main(): # Main function, activated when sequencer launched
     sequencer.buttonDecrOct.when_pressed = decrease_pitch # TODO ?
     '''
     
-    sequencer.buttonDecrOct.when_pressed = lcd.test #?
-
-    sequencer.button1.when_pressed = on # TODO
+    #* sequencer.button1.when_pressed = ?.on # TODO
     sequencer.buttonIncrOct.when_pressed = sequencer.noteSequence.increaseOctave #?
-    sequencer.buttonDecrOct.when_pressed = sequencer.noteSequence.decreaseOctave #?
+    #!sequencer.buttonDecrOct.when_pressed = sequencer.noteSequence.decreaseOctave #?
+    sequencer.buttonDecrOct.when_pressed = sequencer.noteSequence.increasePitch #!
     #* sequencer.buttonHearNote.when_pressed = sequencer.playNote
 
     sequencer.rotorPitch.when_rotated_clockwise = sequencer.noteSequence.increasePitch
@@ -91,21 +81,6 @@ def main(): # Main function, activated when sequencer launched
     sequencer.rotorStep.when_rotated_clockwise = sequencer.noteSequence.increaseStep
     sequencer.rotorStep.when_rotated_counter_clockwise = sequencer.noteSequence.decreaseStep
 
-    '''
-    for i in range(5):
-        time.sleep(10)
-        print("TEST")
-    #raise SystemExit
-
-    a = True
-    while a == True:
-        gate.sendGateSignal
-        sleep((60/tempo.value)*gate.value)
-        gate.stop
-        sleep((60/tempo.value)*(1-gate.value))
-    '''
-    
-
     # sequence led, sequence gate send following tempo
 
     ''' for testing :
@@ -116,68 +91,105 @@ def main(): # Main function, activated when sequencer launched
             sequencer.dac1.setVoltage(0, pitchDAC)
             time.sleep(60/tempo.value)
     '''
+    #gpiozero.pause()
+    #time.sleep(15)
+    #raise SystemExit
+    
+    #lcd.clearRightLCD()
     while True: # works at the same time as the rotary encoders are turning and changing values
         print("testing")
-        time.sleep(5)
-        on()
+        time.sleep(3)
 
-def setvoltage():
-    #dac3.setVoltage(1, 12*l_step[tempo.n][0] + l_step[tempo.n][1])
-    print("GATE ON TEST")
-    #dac3.setVoltage(1, 0)
-    print(tempo.step)
-
-def off():
-    tempo.on = "off"
-
-def setvoltage2():
-    print("TEST GATE dac ON")
-    time.sleep((60/tempo.value)*(gate.value))
-    print("TEST GATE dac OFF")
-    time.sleep((60/tempo.value)*(1-gate.value))
-
-def on():
-    tempo.on = "on"
-    while tempo.on == "on":
-        t1 = threading.Timer((60/tempo.value)*(1-gate.value), setvoltage)
-        t1.run()
-        time.sleep((60/tempo.value)*(gate.value))
-        t2 = threading.Thread(setvoltage2)
-        t2.run()
-        #dac3.setVoltage(1, 0)
-        print("Set Voltage test")
-        tempo.step += 1
-        tempo.step = tempo.step%8
-        time.sleep(15)#!!!
-        off()
-        if sequencer.button1.is_pressed == False:
-            off()
-        #button1.when_released = off
 
 def endSequencer():
     # Set the DACs to 0V and turns the LCD backlight off (executed when keyboard interrupt or other)
     global sequencer
+    #sequencer = Sequencer() # ?
     global lcd
-    for dac in sequencer.dacs:
-        for i in range(2):
-            dac.output.setVoltage(i, 0)
-            print("---DAC", dac.number,"channel",i,"set to 0---")
+    #lcd = Simulatelcd() # ?
+    for i in range(2):
+        sequencer.dac1.setVoltage(i, 0)
+        sequencer.dac2.setVoltage(i, 0)
+        sequencer.dac3.setVoltage(i, 0)
+    lcd.fullClearLCD()
     lcd.toggleBacklight(False)
-    # print("LCD Backlight turned OFF")
-    print("==========Sequencer ended==========")
 
 
 if __name__ == "__main__":
     # launch main() if this file is launched (SequencerMain.py)
     try:
         main()
-    except (KeyboardInterrupt, SystemExit) as error:  # catches a keyboard interrupt or a raised system exit (raise KeyboardInterrupt OR raise SystemExit("_Ending program_") OR sys.exit("_Ending program_"))
-        print("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\n"+error.__class__.__name__ +
-              ", stopping sequencer\n\/\/\/\/\/\/\/\/\/\/\/\/\/\/")
-        #endSequencer() # stops the sequencer
+    except (KeyboardInterrupt, SystemExit):  # catches a keyboard interrupt
+        print("Keyboard interrupt, stopping sequencer")
     finally:
-        print("========Ending Sequencer========")
         endSequencer() # stops the sequencer
         sys.exit(0)
 
+
+
+'''
+class Tempo: # needs to be changed ?
+    # Tempo
+    def __init__(self, current_tempo):
+        self.current_tempo = current_tempo
+    def changeTempo(self, new_tempo):
+        self.current_tempo=new_tempo
+
+class Clock: # needs to be changed
+    # Clock
+    def __init__(self):
+        pass
+    def ticking(self, tempo):
+        print("tick")
+        clock=0
+        #time.sleep(60/self.tempo.current_tempo)
+        #send a clock signal
+        print("tack")
+        clock=1
+        time.sleep(60/tempo.current_tempo)
+'''
+
+'''
+    # End of program:
+    sequencer.dac1.setVoltage(0, 0)
+    sequencer.dac2.setVoltage(0, 0)
+    sequencer.dac3.setVoltage(0, 0)
+    lcd.toggleBacklight(False)
+'''
+
+'''
+    lcd = Simulatelcd() # initialization of a simulated LCD, to use when running the program outside of a RPi, it will just print out rather than display on the LCD
+    #lcd = LCDSequencer.LCDSequencer() # LCD initialization
+    tempo = Tempo(60) # Tempo initialization (default 60 bpm)
+    #clock = Clock(tempo) # Clock initialization
+    clock = ClockSequencer() # Clock initialization
+    ledSequence = LEDSequence(8) # LEDs Sequence initialization (8 LEDs)
+
+    #lcd.toggleBacklight(True) # Turns the backlight ON
+    lcd.displayTempo(tempo.current_tempo) # shows the current tempo
+
+    for i in range(1,5):
+        # Start of the tick tack sequence (10 times)
+        clock.ticking(tempo)
+    
+    tempo.changeTempo(120)
+    lcd.displayTempo(tempo.current_tempo)
+
+    for i in range(1,5):
+        # Start of the tick tack sequence (10 times)
+        clock.ticking(tempo)
+
+    lcd.toggleBacklight(False) # Turns the backlight OFF for the end of the program
+'''
+
+''' Test code on RPi
+    lcd.displayNote("A#",4)
+    lcd.displayTempo(100)
+    step7 = Step(7)
+    time.sleep(5)
+    lcd.displayStep(step7)
+    time.sleep(5)
+    lcd.clearLCD()
+    lcd.toggleBacklight(False)
+'''
 
