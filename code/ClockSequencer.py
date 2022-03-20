107c2628d18922746908f2e3fad4b849bbc12fb1
Lucas Placentino <23436953+lucasplacentino@users.noreply.github.com>
2022-03-21 00:40:16 +0100

    fixed clock class
    forgot self.sequencer.dac1.output.open() etc

--- code/ClockSequencer.py
+++ code/ClockSequencer.py
@@ -22,7 +22,9 @@ class ClockSequencer():
             pitch = self.sequencer.noteSequence.listSteps[self.tempo.step][1]
             octave = self.sequencer.noteSequence.listSteps[self.tempo.step][0]
             # change note each step
+            self.sequencer.dac1.output.open()
             self.sequencer.dac1.output.setVoltage(PITCH_CHANNEL, int(MAX_DAC*((12*int(octave) + int(pitch))/NB_NOTES))) # ! WHICH DAC ?
+            self.sequencer.dac1.output.close()
             print("step", self.tempo.step, "note to dac1 channel0:", pitch, octave)        
         
             print("----step start----")
@@ -30,18 +32,23 @@ class ClockSequencer():
             self.sequencer.ledSequence.ledOn(self.tempo.step)
         
             # start gate
+            self.sequencer.dac2.output.open()
             self.sequencer.dac2.output.setVoltage(GATE_CHANNEL, MAX_DAC) # ! WHICH DAC ?
+            self.sequencer.dac2.output.close() # ?
             print("Gate value:", self.gate.value)
             time.sleep((60/self.tempo.value)*(self.gate.value)) # ?
         
             # end gate
+            self.sequencer.dac2.output.open() # ?
             self.sequencer.dac2.output.setVoltage(GATE_CHANNEL, 0) # ! WHICH DAC ?
+            self.sequencer.dac2.output.close()
             time.sleep((60/self.tempo.value)*(1-self.gate.value)) # ?
         
             print("----step ended----")
 
             self.tempo.step += 1
             self.tempo.step = self.tempo.step%NB_STEPS
+            
             if self.sequencer.button1.is_pressed == False:
                 self.pauseClock()
 
